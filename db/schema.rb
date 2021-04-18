4567aa817c12551aef2dc24b72b0cb1cf351799e
Nicol Drozdowicz <nicoldrozdowicz@Nicols-MacBook-Pro-2.local>
2021-04-18 13:13:45 -0400

    Migrations and the resulting Schema

---
+++ db/schema.rb
@@ -0,0 +1,18 @@
+ActiveRecord::Schema.define(version: 2021_04_18_171128) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
